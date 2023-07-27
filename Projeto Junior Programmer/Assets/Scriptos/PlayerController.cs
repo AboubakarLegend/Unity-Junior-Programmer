845106daeb9f226ce734ef4559aec4c861ad2f5d
AboubakarLegend <nicolas.nero0dmc@gmail.com>
2023-07-26 21:32:45 -0300

    o_cara_eh_bom

--- Projeto Junior Programmer/Assets/Scriptos/PlayerController.cs
+++ Projeto Junior Programmer/Assets/Scriptos/PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject tank;
-    private float speed = 2.0f;
+    private float speed = 0.0f;
+    private float turnspeed = 5.0f;
     void Start()
     {
         
@@ -27,7 +29,30 @@ public class PlayerController : MonoBehaviour
         //    p = new Vector3(p.x, p.y, p.z-1);
         //    tank.transform.position = p;
         //}
-        transform.Translate(Vector3.forward *Time.deltaTime * speed);
-        speed = speed + 0.04f;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            speed = speed + 0.02f;
+        }
+        else
+        {
+            if(speed <= 0)
+            {
+                speed=0;
+            }
+            else if (speed > 0)
+            {
+                speed = speed - 0.04f;
+            }
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * turnspeed);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * turnspeed);
+        }
     }
 }
